3ef1880e10a8064f334e0f67ee64ef1649ec30e5
DucViet <vietduc.tran@apuscorp.com>
2023-04-12 14:18:18 +0700

    create api upload file

--- src/main/java/com/kma/project/chatapp/controller/NewController.java
+++ src/main/java/com/kma/project/chatapp/controller/NewController.java
@@ -4,11 +4,14 @@ import com.kma.project.chatapp.dto.request.NewRequestDto;
 import com.kma.project.chatapp.dto.response.NewResponseDto;
 import com.kma.project.chatapp.dto.response.PageResponse;
 import com.kma.project.chatapp.service.NewService;
+import com.kma.project.chatapp.service.UploadFileService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 
@@ -20,6 +23,9 @@ public class NewController {
 
     @Autowired
     NewService newService;
+
+    @Autowired
+    UploadFileService uploadFileService;
 
     @ApiOperation(value = "Thêm mới bài viết")
     @PostMapping
@@ -49,6 +55,12 @@ public class NewController {
     @GetMapping("/detail/{id}")
     public ResponseEntity<?> getDetailUser(@PathVariable("id") Long id) {
         return ResponseEntity.ok(newService.getDetail(id));
+    }
+
+    @ApiOperation(value = "Thêm mới ảnh")
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, value = "/upload")
+    public String add(@RequestPart("file") MultipartFile file) {
+        return uploadFileService.uploadFileCloud(file);
     }
 
 
